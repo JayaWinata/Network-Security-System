4de99854a373cb51d72931e60315621df2e8bd0c
Jaya Winata <jayawinata100@gmail.com>
2025-06-08 21:05:37 +0700

    feat: add data validation pipeline / component

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from src.components.data_ingestion import DataIngestion
+from src.components.data_validation import DataValidation
 from src.exception.exception import NetworkSecurityException
-from src.entity.config_entity import DataIngestionConfig
-from src.entity.config_entity import TrainingPipelineConfig
+from src.entity.config_entity import DataIngestionConfig, DataValidationConfig, TrainingPipelineConfig
 from src.logging.logger import logger
 
 import sys
@@ -13,6 +13,13 @@ if __name__ == "__main__":
         data_ingestiion = DataIngestion(data_ingestion_config)
         logger.info("Initiate the data ingestion...")
         data_ingestion_artifact = data_ingestiion.initiate_data_ingestion()
+        logger.info("Data ingestion step completed")
         print(data_ingestion_artifact)
+        data_validation_config=DataValidationConfig(training_pipeline_config)
+        data_validation=DataValidation(data_ingestion_artifact,data_validation_config)
+        logger.info("Initiate the data Validation...")
+        data_validation_artifact=data_validation.initiate_data_validation()
+        logger.info("Data validation step completed")
+        print(data_validation_artifact)
     except Exception as e:
         raise NetworkSecurityException(e, sys)
--- src/constant/__init__.py
+++ src/constant/__init__.py
@@ -14,6 +14,8 @@ FILE_NAME = 'Dataset Phising Website.csv'
 TRAIN_FILE_NAME = 'train.csv'
 TEST_FILE_NAME = 'test.csv'
 
+SCHEMA_FILE_PATH = os.path.join('data_schema', 'schema.yaml')
+
 '''
 Data Ingestion Constants
 '''
@@ -23,3 +25,12 @@ DATA_INGESTION_DIR_NAME = 'data_ingestion'
 DATA_INGESTION_FEATURE_STORE_DIR = 'feature_store'
 DATA_INGESTION_INGESTED_DIR = 'ingested'
 DATA_INGESTION_TEST_RATIO = 0.2
+
+'''
+Data Validation Constants
+'''
+DATA_VALIDATION_DIR_NAME = 'data_validation'
+DATA_VALIDATION_VALID_DIR = 'valid'
+DATA_VALIDATION_INVALID_DIR = 'invalid'
+DATA_VALIDATION_DRIFT_REPORT_DIR = 'drift_report'
+DATA_VALIDATION_DRIFT_REPORT_FILENAME = 'report.yaml'
