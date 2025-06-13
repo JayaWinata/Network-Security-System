400d75bdf015b15f6ecfc31b12f0a26e42c0e2b4
Jaya Winata <jayawinata100@gmail.com>
2025-06-13 14:17:08 +0700

    feat: set dagshub env directly to the mlflow

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -18,6 +18,11 @@ os.environ["MLFLOW_TRACKING_URI"]= os.getenv('MLFLOW_TRACKING_URI')
 os.environ["MLFLOW_TRACKING_USERNAME"]= os.getenv('MLFLOW_TRACKING_USERNAME')
 os.environ["MLFLOW_TRACKING_PASSWORD"]= os.getenv('MLFLOW_TRACKING_PASSWORD')
 
+
+mlflow.set_tracking_uri(os.getenv("MLFLOW_TRACKING_URI"))
+mlflow.set_tracking_username(os.getenv("MLFLOW_TRACKING_USERNAME"))
+mlflow.set_tracking_password(os.getenv("MLFLOW_TRACKING_PASSWORD"))
+
 import dagshub
 dagshub.init(repo_owner='jayawinata100', repo_name='Network-Security-System', mlflow=True)
 
